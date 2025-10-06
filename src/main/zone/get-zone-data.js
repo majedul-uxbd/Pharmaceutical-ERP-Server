4fbf25aef9c49c019a70f8e94c6aab309dc198a5
majedul-uxbd <majedul@uxd.co.jp>
2025-10-06 11:05:51 +0600

    mod: Made modification zone roter

--- src/main/zone/get-zone-data.js
+++ src/main/zone/get-zone-data.js
@@ -10,6 +10,10 @@
  */
 
 const { pool } = require("../../_DB/db");
+const { TABLE_DEPOT_INFO_COLUMNS_NAME } = require("../../_DB/DB-table-info/table-depot-info-column-name");
+const { TABLE_EMPLOYEES_COLUMNS_NAME } = require("../../_DB/DB-table-info/table-employee-column-name");
+const { TABLE_ZONE_COLUMNS_NAME } = require("../../_DB/DB-table-info/table-zone-column-name");
+const { TABLES } = require("../../_DB/DB-table-info/tables-name.const");
 const { API_STATUS_CODE } = require("../../consts/error-status");
 const { setServerResponse } = require("../../utilities/server-response");
 
@@ -18,7 +22,7 @@ const getNumberOfRowsQuery = async () => {
     SELECT
         count(*) totalRows
     FROM
-        zone;
+        ${TABLES.TBL_ZONE};
     `;
 
     try {
@@ -32,23 +36,33 @@ const getNumberOfRowsQuery = async () => {
 
 const getZoneDataQuery = async (paginationData) => {
     const query = `
-        SELECT
-            z.id,
-            z.zone_id,
-            z.zone_code,
-            z.zone_name,
-            z.comment,
-            z.zone_status,
-            di.depot_name,
-            z.created_at,
-            z.modified_at
+        SELECT DISTINCT
+            zone.${TABLE_ZONE_COLUMNS_NAME.ID},
+            zone.${TABLE_ZONE_COLUMNS_NAME.ZONE_ID},
+            zone.${TABLE_ZONE_COLUMNS_NAME.ZONE_CODE},
+            zone.${TABLE_ZONE_COLUMNS_NAME.ZONE_NAME},
+            zone.${TABLE_ZONE_COLUMNS_NAME.COMMENT},
+            zone.${TABLE_ZONE_COLUMNS_NAME.ACTIVE_STATUS},
+            zone.${TABLE_ZONE_COLUMNS_NAME.CREATED_AT},
+            zone.${TABLE_ZONE_COLUMNS_NAME.MODIFIED_AT},
+            created_by.${TABLE_EMPLOYEES_COLUMNS_NAME.Full_NAME} AS created_by,
+            modified_by.${TABLE_EMPLOYEES_COLUMNS_NAME.Full_NAME} AS modified_by,
+            depot.${TABLE_DEPOT_INFO_COLUMNS_NAME.DEPOT_NAME}
         FROM
-            zone AS z
-        LEFT JOIN depot_info AS di
+            ${TABLES.TBL_ZONE} AS zone
+        LEFT JOIN ${TABLES.TBL_DEPORT_INFO} AS depot
         ON
-            z.depot_id = di.depot_id
+            zone.${TABLE_ZONE_COLUMNS_NAME.DEPOT_ID} = depot.${TABLE_DEPOT_INFO_COLUMNS_NAME.DEPOT_ID}
+        LEFT JOIN
+            ${TABLES.TBL_EMPLOYEES} AS created_by
+        ON
+            zone.${TABLE_ZONE_COLUMNS_NAME.CREATED_BY} = created_by.${TABLE_EMPLOYEES_COLUMNS_NAME.EMPLOYEE_ID}
+        LEFT JOIN
+            ${TABLES.TBL_EMPLOYEES} AS modified_by
+        ON
+            zone.${TABLE_ZONE_COLUMNS_NAME.MODIFIED_BY} = modified_by.${TABLE_EMPLOYEES_COLUMNS_NAME.EMPLOYEE_ID}
         ORDER BY
-            z.id
+            zone.${TABLE_ZONE_COLUMNS_NAME.ID}
         DESC
         LIMIT ? OFFSET ?;
     `;
